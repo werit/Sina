0238db872ff12f00b404dddbad90c7758cc63298
Tibor Balaz Pc <tib.balaz@gmail.com>
2021-07-14 10:30:08 +0200

    Added test project with first test

---
+++ recipeAPITest/Dto/ResponseMessageDto.cs
@@ -0,0 +1,20 @@
+﻿using recipeAPITest.TestUtils;
+using recipies_ms.Web.Dto;
+using Xunit;
+
+namespace recipeAPITest.Dto
+{
+    public class ResponseMessageDtoTest
+    {
+        [Trait(TestCategories.TraitName, TestCategories.UnitTest)]
+        [Theory]
+        [InlineData("some random message")]
+        [InlineData("")]
+        [InlineData(null)]
+        public void ResponseMessageDtoBasicTest(string message)
+        {
+            var respMessage = new ResponseMessageDto {Message = message};
+            Assert.Equal(message, respMessage.Message);
+        }
+    }
+}
---
+++ recipeAPITest/TestUtils/TestCategories.cs
@@ -0,0 +1,11 @@
+﻿namespace recipeAPITest.TestUtils
+{
+    public static class TestCategories
+    {
+        public const string TraitName = "Category";
+
+        public const string UnitTest = "UnitTest";
+        public const string IntegrationTest = "IntegrationTest";
+        public const string EndToEndTest = "EndToEndTest";
+    }
+}
