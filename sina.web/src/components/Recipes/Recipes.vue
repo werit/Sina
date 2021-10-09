bac465dcd9173d12b6c2ee488e397aa4e271286a
Tibor Balaz Pc <tib.balaz@gmail.com>
2021-10-10 00:44:43 +0200

    Added vuetify for material design

---
+++ sina.web/src/components/Recipes/Recipes.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <v-card color="grey lighten-4" flat height="50px" tile>
+      <v-toolbar dense>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+        <v-toolbar-title>Search</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>mdi-heart</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+      </v-toolbar>
+    </v-card>
+    <v-container>
+      <v-row>
+        <div>
+          <v-container grid-list-md>
+            <recipes-list />
+          </v-container>
+        </div>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import RecipesList from './RecipesList2.vue';
+
+export default {
+  name: 'Recipes',
+  data: () => ({}),
+  components: { RecipesList },
+  methods: {},
+};
+</script>
