b8baf37e9e431db9e801d868890bdb1dca93bc8b
Werit_HP_Spectre <tib.balaz@gmail.com>
2021-12-18 00:05:16 +0000

    Added RecipeScheduleCreateDto

---
+++ sina.planning/Web/Dto/RecipeScheduleCreateDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sina.planning.Web.Dto
+{
+    // ReSharper disable once InconsistentNaming
+    public class RecipeScheduleCreateDto
+    {
+        public DateTime RecipeScheduleTime { get; set; }
+        public Guid RecipeKey { get; set; }
+        public float RecipePortions { get; set; }
+    }
+}
