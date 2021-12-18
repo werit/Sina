6e5a28b2a709814a47511a9f8e1d299d11faf9f7
Werit_HP_Spectre <tib.balaz@gmail.com>
2021-12-18 00:05:39 +0000

    Added RecipeScheduleController

---
+++ sina.planning/Controllers/RecipeScheduleController.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using sina.planning.Db;
+using sina.planning.Db.Models;
+using sina.planning.Web.Dto;
+
+namespace sina.planning.Controllers
+{
+    [ApiController]
+    [Route("api/v1/system/schedule")]
+    public class RecipeScheduleController : ControllerBase
+    {
+        private readonly ILogger<RecipeScheduleController> logger;
+        private readonly IRecipeSchedulingDbContext<RecipeScheduleItem> schedulingDbContext;
+
+        public RecipeScheduleController(ILogger<RecipeScheduleController> logger,
+            IRecipeSchedulingDbContext<RecipeScheduleItem> schedulingDbContext)
+        {
+            this.logger = logger;
+            this.schedulingDbContext = schedulingDbContext;
+        }
+
+        [HttpPost("add")]
+        public async Task<ActionResult<RecipeScheduleItemDto>> AddRecipeSchedule(
+            RecipeScheduleCreateDto scheduleCreateDto, CancellationToken cancellationToken)
+        {
+            var scheduleItem = new RecipeScheduleItem
+            {
+                RecipeKey = scheduleCreateDto.RecipeKey,
+                RecipePortions = scheduleCreateDto.RecipePortions,
+                RecipeScheduleTime = scheduleCreateDto.RecipeScheduleTime
+            };
+            var recipeScheduleItemDto =
+                (await schedulingDbContext.AddRecipeScheduleAsync(scheduleItem, cancellationToken))
+                .ToRecipeScheduleItemDto();
+            return Created("",recipeScheduleItemDto);
+        }
+    }
+}
