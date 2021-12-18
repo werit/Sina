1a3d5615b8b8d96d730d154e053b4b013577ca03
Werit_HP_Spectre <tib.balaz@gmail.com>
2021-12-18 00:04:47 +0000

    Added RecipeScheduleItemDto

---
+++ sina.planning/Web/Dto/RecipeScheduleItemDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using sina.planning.Db.Models;
+
+namespace sina.planning.Web.Dto
+{
+    public class RecipeScheduleItemDto
+    {
+        public Guid RecipeScheduleKey { get; set; }
+        public DateTime RecipeScheduleTime { get; set; }
+        public string RecipeName { get; set; }
+        public float RecipePortions { get; set; }
+    }
+
+    public static class RecipeScheduleItemExtension
+    {
+        
+        public static RecipeScheduleItemDto ToRecipeScheduleItemDto(this RecipeScheduleItem  recipeScheduleItem)
+        {
+            var recipeScheduleItemDto = new RecipeScheduleItemDto
+            {
+                RecipeScheduleKey = recipeScheduleItem.RecipeScheduleKey,
+                RecipeScheduleTime = recipeScheduleItem.RecipeScheduleTime,
+                RecipeName = recipeScheduleItem.RecipeName,
+                RecipePortions = recipeScheduleItem.RecipePortions,
+            };
+            return recipeScheduleItemDto;
+        }
+    }
+}
